26da9bff3090708c16e99c0e118367d9323da3fa
haupti <haupti@cip.ifi.lmu.de>
2021-10-13 22:26:29 +0200

    structure for Dijkstraalgo adding functions to create connections and change them

--- Dijkstra/src/modell/Dijkstraalgo.java
+++ Dijkstra/src/modell/Dijkstraalgo.java
@@ -5,20 +5,21 @@ import java.util.List;
 
 public class Dijkstraalgo {
 
-  List<Node> nodes = new ArrayList<>();
-
   public static class Node {
-    public Node(String name, List<Cost> costs) {
+    public Node(String name) {
       this.name = name;
-      this.costs = costs;
+    }
+
+    public List<Connection> getCosts() {
+      return costs;
     }
 
     String name;
-    List<Cost> costs;
+    List<Connection> costs = new ArrayList<>();
   }
 
-  public static class Cost {
-    public Cost(Node node, int cost) {
+  public static class Connection {
+    public Connection(Node node, int cost) {
       this.node = node;
       this.cost = cost;
     }
@@ -27,12 +28,27 @@ public class Dijkstraalgo {
     int cost;
   }
 
-  public void addNode(){
-
+  public void addConnection(Node n, Node z, int cost){
+    n.getCosts().add(new Connection(z, cost));
   }
 
+  public void searchConnection(Node n, Node z){
+    n.getCosts().add(new Connection(z, cost));
+  }
+
+  public void deleteConnection(Node n, Node z){
+    //if it exists
+    n.getCosts().add(new Connection(z, cost));
+  }
+
+  public void changeCostOfConnection(Node n, Node z){
+    //if it exists
+    n.getCosts().add(new Connection(z, cost));
+  }
 
   public static void main(String[] args) {
     Dijkstraalgo path = new Dijkstraalgo();
+
+
   }
 }
