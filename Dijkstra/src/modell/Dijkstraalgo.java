87099ec2f7c6fc83ab73fa4c3cacab95163c2530
haupti <haupti@cip.ifi.lmu.de>
2021-10-13 22:40:32 +0200

    structure for Dijkstraalgo adding functions to create connections and change them

--- Dijkstra/src/modell/Dijkstraalgo.java
+++ Dijkstra/src/modell/Dijkstraalgo.java
@@ -26,29 +26,49 @@ public class Dijkstraalgo {
 
     Node node;
     int cost;
+
+    public void setCost(int cost) {
+      this.cost = cost;
+    }
   }
 
-  public void addConnection(Node n, Node z, int cost){
+  public void addConnection(Node n, Node z, int cost) {
     n.getCosts().add(new Connection(z, cost));
   }
 
-  public void searchConnection(Node n, Node z){
-    n.getCosts().add(new Connection(z, cost));
+  public Connection searchConnection(Node n, Node z) {
+    if (n.costs == null) {
+      return null;
+    } else {
+      for (Connection s : n.getCosts()) {
+        if (s.node.equals(z)) {
+          return s;
+        } else {
+          return null;
+        }
+      }
+    }
   }
 
-  public void deleteConnection(Node n, Node z){
-    //if it exists
-    n.getCosts().add(new Connection(z, cost));
+  public void deleteConnection(Node n, Node z) {
+    Connection c = searchConnection(n, z);
+    if (c != null) {
+      n.getCosts().remove(c);
+    }
   }
 
-  public void changeCostOfConnection(Node n, Node z){
-    //if it exists
-    n.getCosts().add(new Connection(z, cost));
+  public void changeCostOfConnection(Node n, Node z, int newCost) {
+    Connection c = searchConnection(n, z);
+    if (c != null) {
+      for (Connection s : n.getCosts()) {
+        if (s.equals(c)) {
+          s.setCost(newCost);
+        }
+      }
+    }
   }
 
   public static void main(String[] args) {
     Dijkstraalgo path = new Dijkstraalgo();
-
-
   }
 }
