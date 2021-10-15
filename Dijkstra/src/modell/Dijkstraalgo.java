2e7e302dd48d8d5cb389c73debdcc3754df79ec7
haupti <haupti@cip.ifi.lmu.de>
2021-10-15 20:54:58 +0200

    Dijkstra logic being badly written

--- Dijkstra/src/modell/Dijkstraalgo.java
+++ Dijkstra/src/modell/Dijkstraalgo.java
@@ -1,20 +1,19 @@
 package modell;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.Predicate;
+import javax.imageio.ImageIO;
 
 public class Dijkstraalgo {
 
   List<Node> graph = new ArrayList<>();
 
   public Node getNode(final String name) {
-    return graph
-            .stream()
-            .filter(node -> name.equalsIgnoreCase(node.name))
-            .findAny()
-            .orElseThrow();
+    return graph.stream().filter(node -> name.equalsIgnoreCase(node.name)).findAny().orElseThrow();
   }
 
   public static class Node {
@@ -39,9 +38,7 @@ public class Dijkstraalgo {
 
     @Override
     public String toString() {
-      return "Node{" +
-             "name='" + name + '\'' +
-             '}';
+      return "Node{" + "name='" + name + '\'' + '}';
     }
 
     @Override
@@ -87,8 +84,31 @@ public class Dijkstraalgo {
 
     @Override
     public String toString() {
-      return node.name +
-              " cost=" + cost;
+      return node.name + " cost=" + cost;
+    }
+  }
+
+  public class Path {
+    // the node is the goal, and the list is the path from start to goal
+
+    public Path(Node node) {
+      this.node = node;
+    }
+
+    public List<Node> getPath() {
+      return path;
+    }
+
+    Node node;
+    List<Node> path;
+    int gesamtCost;
+
+    public void setPath(List<Node> path) {
+      this.path = path;
+    }
+
+    public void setGesamtCost(int gesamtCost) {
+      this.gesamtCost = gesamtCost;
     }
   }
 
@@ -146,11 +166,74 @@ public class Dijkstraalgo {
     }
   }
 
+  private BufferedImage getImage(String filename) {
+    // This time, you can use an InputStream to load
+    try {
+      // Grab the InputStream for the image.
+      InputStream in = getClass().getResourceAsStream(filename);
+      // Then read it.
+      return ImageIO.read(in);
+    } catch (IOException e) {
+      System.out.println("The image was not loaded.");
+      // System.exit(1);
+    }
+    return null;
+  }
+
   public void drawConnections() {
     for (Node n : graph) {
       System.out.println(n.name + " ->" + n.connections);
     }
   }
+
+  public void imageToNodes() {}
+
+  public Path myDijkstra(Node start, Node ziel) {
+    List<Path> paths = new ArrayList<>();
+    List<Node> nichtBearbeitet = graph;
+
+    if (start != null && ziel != null) {
+      if (graph.contains(start) && graph.contains(ziel)) {
+        for (Node n : graph) {
+          if (n.equals(start)) {
+            // remove the start node, as we don't need to visit it
+            nichtBearbeitet.remove(start);
+            // create paths for every single node except the start
+            for (int i = 0; i < nichtBearbeitet.size(); i++) {
+              paths.set(i, new Path(nichtBearbeitet.get(i)));
+            }
+            // update the paths for the neighbors of the start node
+            for (int i = 0; i < n.connections.size(); i++) {
+              for (Path a : paths) {
+                if (start.connections.get(i).node.equals(a.node)) {
+                  a.getPath().add(n);
+                  a.setGesamtCost(n.connections.get(i).cost);
+                }
+              }
+            }
+            while (nichtBearbeitet.size() >= 1) {
+              // compare the costs for the paths that haven't been optimized yet
+              Node minNode = nodeWithMinCost(paths, nichtBearbeitet);
+              // remove the chosen node, as its path is now optimal
+              nichtBearbeitet.remove(minNode);
+              for (int i = 0; i < minNode.connections.size(); i++) {
+                for (Path a : paths) {
+                  if (minNode.connections.get(i).node.equals(a.node)) {
+                    //TODO: here I need to first compare the costs of the paths if bigger replace, if smaller leave alone
+                    a.getPath().add(minNode);
+                    a.setGesamtCost(minNode.connections.get(i).cost);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  private Node nodeWithMinCost(List<Path> paths, List<Node> nichtBearbeitet) {}
 
   public static void main(String[] args) {
     Dijkstraalgo graph = createDemoGraph();
